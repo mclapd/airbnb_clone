fea5b69b9ef4a7bfd8be707d7dd769dbeca39658
one_punch_man <ckim@banlaw.com>
2023-10-09 17:36:17 +1100

    build ok

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FieldError, FieldValues, UseFormRegister } from "react-hook-form";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 import { BiDollar } from "react-icons/bi";
 
 interface InputProps {
@@ -11,7 +11,7 @@ interface InputProps {
   formatPrice?: boolean;
   required?: boolean;
   register: UseFormRegister<FieldValues>;
-  errors: FieldError;
+  errors: FieldErrors;
 }
 
 const Input: React.FC<InputProps> = ({
@@ -39,15 +39,16 @@ const Input: React.FC<InputProps> = ({
         placeholder=" "
         type={type}
         className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed
-        ${formatPrice ? "pl-9" : "pl-4"}
-        ${errors[id] ? "border-rose-500" : "border-neutral-300"}
+        ${formatPrice ? "pl-9" : "pl-4"} 
+        ${errors[id] ? "border-rose-500" : "border-neutral-300"} 
         ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
         `}
       />
       <label
-        className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0]
-        ${formatPrice ? "left-9" : "left-4"}
-        peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4
+        className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] 
+        ${
+          formatPrice ? "left-9" : "left-4"
+        } peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 
         ${errors[id] ? "text-rose-500" : "text-zinc-400"}`}
       >
         {label}
